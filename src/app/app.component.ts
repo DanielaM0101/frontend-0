f15a2eb08135bef36fe35dd1f31ccce5e9501cec
danny <kdmunoz@sudamericano.edu.ec>
2024-06-13 09:29:54 -0500

    :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,64 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    NzTableModule,
+    NzButtonModule,
+    FormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzModalModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-0';
+  products: any[] = [];
+  isVisible = false;
+  currentProduct: any = {};
+
+  constructor(private http: HttpClient) {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.http.get<any[]>('http://localhost:3000/products').subscribe(data => {
+      this.products = data;
+    });
+  }
+
+  showModal(product?: any): void {
+    this.currentProduct = product ? { ...product } : {};
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    if (this.currentProduct.id) {
+      this.http.put(`http://localhost:3000/products/${this.currentProduct.id}`, this.currentProduct)
+        .subscribe(() => this.loadProducts());
+    } else {
+      this.http.post('http://localhost:3000/products', this.currentProduct)
+        .subscribe(() => this.loadProducts());
+    }
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  deleteProduct(id: number): void {
+    this.http.delete(`http://localhost:3000/products/${id}`).subscribe(() => this.loadProducts());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { NZ_I18N, es_ES } from 'ng-zorro-antd/i18n';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_ICONS, NzIconModule } from 'ng-zorro-antd/icon';
+import { PlusOutline, EditOutline, DeleteOutline } from '@ant-design/icons-angular/icons';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Importa solo los íconos necesarios
+const icons = [
+  PlusOutline,
+  EditOutline,
+  DeleteOutline,
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withFetch()),
+    importProvidersFrom(BrowserAnimationsModule, NzIconModule),
+    { provide: NZ_I18N, useValue: es_ES },
+    { provide: NZ_ICONS, useValue: icons }
+  ]
+});
